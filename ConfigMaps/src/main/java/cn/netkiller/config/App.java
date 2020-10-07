8fa3132a31d476805863950317b03ddfc56e9bb2
Neo Chen <netkiller@msn.com>
2020-10-07 19:53:24 +0800

    kubernetes

--- ConfigMaps/src/main/java/cn/netkiller/config/App.java
+++ ConfigMaps/src/main/java/cn/netkiller/config/App.java
@@ -1,11 +1,11 @@
-package cn.netkiller.service;
+package cn.netkiller.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
