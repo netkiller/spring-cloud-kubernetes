ea53ca439514d1d44d76afae0d3e7105ad643f0c
Neo Chen <netkiller@msn.com>
2020-10-09 13:58:38 +0800

    kubernetes

--- service/src/main/java/cn/netkiller/service/HelloController.java
+++ service/src/main/java/cn/netkiller/service/HelloController.java
@@ -1,28 +1,20 @@
 package cn.netkiller.service;
 
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.slf4j.Slf4j;
 
+@RestController
+@Slf4j
 public class HelloController {
 
-	private static final Log log = LogFactory.getLog(HelloController.class);
+//	private static final Log log = LogFactory.getLog(HelloController.class);
 
-	@Autowired
-	private DiscoveryClient discoveryClient;
-
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String hello() {
+		Log.info("@Slf4j Test OK");
 		return "Hello World";
-	}
-
-	@RequestMapping("/services")
-	public List<String> services() {
-		return this.discoveryClient.getServices();
 	}
 
 }
